528908ca07255fa81c1d488836bbcb873fd9610b
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-05-19 01:19:22 +0200

    test with several filters

--- test.py
+++ test.py
@@ -83,6 +83,35 @@ def test_conv3():
         print('diff:')
         print(diff)
 
+def test_conv4():
+    rand = np.random.rand(6, 6, 1)
+
+    ker1 = np.random.rand(3, 3, 1)
+    ker2 = np.random.rand(3, 3, 1)
+
+    b = miniconv.Conv(2)
+    b.set_kernels([ker1, ker2])
+
+    mine = b.forward(rand)
+    theirs = np.dstack([
+            convolve(rand, ker1, mode='constant'),
+            convolve(rand, ker2, mode='constant')
+            ])
+
+    diff = theirs - mine.squeeze()
+    ok = np.abs(diff).max() < 1e-6
+    if not ok:
+        print(mine.shape)
+        print(theirs.shape)
+        print(rand.squeeze())
+        print('theirs:')
+        print(theirs[:, :, 0])
+        #print(theirs[:,:])
+        print('mine:')
+        print(mine.squeeze())
+        print('diff:')
+        print(diff)
+
 
 def test_conv3d1():
     rand = np.random.rand(6, 6, 3)
@@ -174,10 +203,47 @@ def test_conv3d3():
         print('diff:')
         print(diff)
 
+def test_conv3d4():
+    rand = np.random.rand(6, 6, 3)
+
+    ker1 = np.random.rand(3, 3, 3)
+    ker2 = np.random.rand(3, 3, 3)
+
+    b = miniconv.Conv(2)
+    b.set_kernels([ker1, ker2])
+
+    mine = b.forward(rand)
+    theirs = np.dstack([
+         convolve(rand[:, :, 0], ker1[:, :, 0], mode='constant') +
+         convolve(rand[:, :, 1], ker1[:, :, 1], mode='constant') +
+         convolve(rand[:, :, 2], ker1[:, :, 2], mode='constant'),
+
+         convolve(rand[:, :, 0], ker2[:, :, 0], mode='constant') +
+         convolve(rand[:, :, 1], ker2[:, :, 1], mode='constant') +
+         convolve(rand[:, :, 2], ker2[:, :, 2], mode='constant')
+         ])
+
+    diff = theirs - mine
+    ok = np.abs(diff).max() < 1e-5
+    if not ok:
+        print(mine.shape)
+        print(theirs.shape)
+        print('rand:')
+        print(rand.squeeze())
+        print('theirs:')
+        print(theirs[:, :])
+        #print(theirs[:,:])
+        print('mine:')
+        print(mine.squeeze())
+        print('diff:')
+        print(diff)
+
 
 test_conv1()
 test_conv2()
 test_conv3()
+test_conv4()
 test_conv3d1()
 test_conv3d2()
 test_conv3d3()
+test_conv3d4()
