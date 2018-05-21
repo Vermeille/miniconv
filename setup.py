fad454a78e1b35237d56858c23b702ac79406738
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-05-21 16:15:24 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from distutils.extension import Extension
 setup(
     name="miniconv",
     python_requires='>3.5.2',
+    author='Guillaume "Vermeille" Sanchez',
+    description="A minimalistic deep learning library",
+    author_email='guillaume.v.sanchez@gmail.com',
     ext_modules=[
         Extension(
             "miniconv",
             ["conv.cpp"],
             libraries=["boost_python3", "boost_numpy3"],
-            extra_compile_args=["-ggdb3", "-O0", "-D_GLIBCXX_DEBUG", "-march=native"], )
+            extra_compile_args=["-O3", "-march=native"], )
     ])
